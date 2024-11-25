57aa41f5c1b46bef063464bf6afe91b227822af3
RuthDanielaAguirre <argdiee@icloud.com>
2024-11-25 06:00:52 +0100

    adding RSS & ATOM

--- script.js
+++ script.js
@@ -57,13 +57,59 @@ function cargarXML(){
             const descripcion = objetivo.getElementsByTagName('descripcion')[0].textContent;
 
             const li =document.createElement('li');
-            li.innerHTML = `<strong>Objetivo ${numero}:</strong> ${nombre}-${descripcion}`;
+            li.innerHTML = `Objetivo ${numero}:<br> ${nombre}<br>${descripcion}`;
             listaObjetivos.appendChild(li);
         });
     })
     .catch(error => console.error('Error al cargar XML:', error));
 }
+
+// function marcos(){
+//     fetch('./objetivos.xml')
+//     .then(response => response.text() )
+//     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+//     .then(data => {
+//         const selectElement = document.getElementById("miSelect");
+//         const objetivos = data.getElementsByTagName('objetivo');
+//         Array.from(objetivos).forEach(objetivo =>{
+//             const numero = objetivo.getElementsByTagName('numero')[0].textContent;
+//             const nombre = objetivo.getElementsByTagName('nombre')[0].textContent;
+               
+//             console.log("XML cargado correctamente");
+//           const optionElement = document.createElement("option");
+//           optionElement.value = numero;
+//           optionElement.textContent = nombre;
+//           selectElement.appendChild(optionElement);
+
+
+//           console.log(`Creando opción - Número: ${numero}, Nombre: ${nombre}`); // Confirmación de cada opción creada
+
+//         });
+
+//           selectElement.addEventListener("change", function(){
+//             const valorSeleccionado = selectElement.value;
+          
+//             const contenidoDiv = document.getElementById("contenido");
+//             contenidoDiv.textContent = "";
+
+//             Array.from(objetivos).forEach(objetivo => {
+//                 const numero = objetivo.getElementsByTagName("numero")[0].textContent;
+//                 if(numero===valorSeleccionado){
+//                     const descripcion= objetivo.getElementsByTagName("descripcion")[0].textContent;
+//                     console.log("Descripción encontrada:", descripcion);
+//                     contenidoDiv.textContent =descripcion;
+                   
+//                 }
+//             });
+//           });
+
+
+        
+//     })
+//     .catch(error => console.error('Error al cargar XML:', error));
+// }
     window.onload = function(){
         cargarJSON();
         cargarXML();
+        // marcos();
     };
